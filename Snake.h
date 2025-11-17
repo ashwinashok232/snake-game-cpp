a96a6b91db8a96af41e6cbab0cd998e1eb1e1029
Ashwin Ashok <ashwinashok232@gmail.com>
2025-11-17 14:42:41 -0800

    Add Snake.h

---
+++ Snake.h
@@ -0,0 +1,22 @@
+//
+// Created by Ashwin Ashok on 17/11/25.
+//
+
+#ifndef SNAKE_GAME_CPP_SNAKE_H
+#define SNAKE_GAME_CPP_SNAKE_H
+
+#include <deque>
+
+struct Point {
+    int x;
+    int y;
+};
+
+class Snake {
+public:
+    Snake(int startX, int startY);
+
+    void move(int dx, int dy);
+};
+
+#endif //SNAKE_GAME_CPP_SNAKE_H
